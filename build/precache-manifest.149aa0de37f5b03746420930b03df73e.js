0e6244779a8134f414af357fdfa0b3da6385eecc
Olamide Sholanke <olamidesholanke@MacBook-Pro.local>
2020-09-14 02:20:29 +0100

    build

---
+++ build/precache-manifest.149aa0de37f5b03746420930b03df73e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9bc36cfd5197d9a004226d4cdb67ca8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a29c4d0400d0ca00878",
+    "url": "/static/css/main.036f3cdf.chunk.css"
+  },
+  {
+    "revision": "e181287ed942022abe38",
+    "url": "/static/js/2.a325c713.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a325c713.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a29c4d0400d0ca00878",
+    "url": "/static/js/main.5f0a42c7.chunk.js"
+  },
+  {
+    "revision": "073a6765c5e96cfb09cf",
+    "url": "/static/js/runtime-main.f0a637dc.js"
+  },
+  {
+    "revision": "155f600c5ec2f790f6df327773e1cd77",
+    "url": "/static/media/Arrow.155f600c.svg"
+  },
+  {
+    "revision": "20ed0c754eec0dd7f8b798719cc1c40e",
+    "url": "/static/media/circle.20ed0c75.svg"
+  }
+]);
