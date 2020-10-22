b99e640be92a2396ba7a3630ab260fe965811af9
Olamide Sholanke <olamidesholanke@MacBook-Pro.local>
2020-10-22 15:59:06 +0100

    fix

--- src/components/common/JKLink.jsx
+++ src/components/common/JKLink.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-export default function JKLink({ className, children, href, ...props }) {
-  return (
-    <NavLink {...props} to={href || "/"} className="special-link">
+export default function JKLink({ className, children, to, href, ...props }) {
+  return href ? (
+    <a {...props} href={href || "/"} className="special-link">
+      {children}
+    </a>
+  ) : (
+    <NavLink {...props} to={to || "/"} className="special-link">
       {children}
     </NavLink>
   );
